9e370ec8e4a6a2f5f65e7582cdc85f5d2f722e74
jobanjit-singh <singhjobanjit02@gmail.com>
2023-09-26 23:22:31 +0530

    learn about datatypes in javaScript

---
+++ JavaScript/Basics/dataTypes.js
@@ -0,0 +1,28 @@
+"use script"; //Means we use the latest javaScript
+
+let name = "Jobanjit Singh" //string data type
+let age = 21 //number data type
+
+/*
+Primitive Types:- 
+    string => ex:- "jobanjit"
+    number => ex:- 12 or 2.3
+    boolean => true / false
+    bigint => for bigger number
+    null => standalone value and its type is object
+    undefined => means value is not defined yet
+
+other types:- 
+
+    object
+*/
+// typeof is used to check the type of value 
+// Syntax:- typeof value
+
+let j
+console.log(typeof 23.3)
+console.log(typeof name)
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
+console.log("Example undefined type in js \n"+j)
